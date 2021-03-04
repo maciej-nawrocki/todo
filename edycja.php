974c87ec35bc6de6fe0d8bc197b5f14c725a4928
mef <maciej1709@gmail.com>
2021-03-04 13:09:00 +0100

    fixed update issue

--- edycja.php
+++ edycja.php
@@ -2,12 +2,26 @@
 require_once "db.php";
 $conn = mysqli_connect($host, $user, $pass, $db);
 mysqli_set_charset($conn, "utf8");
+$id = null;
+$title = '';
+$row = [];
 if(isset($_GET['id']))
 {
     $id = $_GET['id'];
     $wpis = "SELECT title FROM tasks WHERE id='$id'";
     $sql = mysqli_query($conn, $wpis);
     $row = mysqli_fetch_assoc($sql);
+    // var_dump($row);die;
+}
+if(isset($_POST['title']) && isset($_POST['id']))
+{
+    $title = $_POST['title'];
+    $row['title'] = $title;
+    $id = $_POST['id'];
+    $add = "UPDATE tasks SET title = '$title' WHERE id ='$id'";
+    mysqli_query($conn, $add);
+    header('location: index.php');
+   
 }
 ?>
 <!DOCTYPE html>
@@ -23,8 +37,12 @@ if(isset($_GET['id']))
     <div class="main">
     <h2>Edycja zadania</h2>
         <div class="add">
-        <form action="index.php" method="post" autocomplete="off">
+        <form action="edycja.php" method="post" autocomplete="off">
             <?php
+            // var_dump($row['title']);die;
+            echo ('<input type="hidden" name="id" value="');
+            echo $_GET['id'];
+            echo '">';
             echo ('<input type="text" name="title" value="');
             echo $row['title'];
             echo '">';
